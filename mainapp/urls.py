520d697a1d02813a50d86ea40b570b176f98ea13
Samir <royal.chaulagain321@gmail.com>
2023-07-06 06:34:00 +0545

    created two page simple blog

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,9 +4,11 @@ from . import views
 # from cureent dir import veiws
 
 urlpatterns = [
-    path('hello/<str:name>',views.home),
-    path('about',views.about,name="about"),
-    path('contact',views.contact)
+    # path('hello/<str:name>',views.home),
+    # path('about',views.about,name="about"),
+    # path('contact',views.contact)
     # path('secondhome',views.secondhome)
+    path('',views.index,name="homepage"),
+    path('create',views.create,name="create")
     
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
-def home(request,name):
-    # name="albert eintein"
-    return render(request,"index.html",{'name':name})
+# def home(request,name):
+#     # name="albert eintein"
+#     return render(request,"index.html",{'name':name})
 
-def about(request):
-    return render(request,"about.html")
-def contact(request):
-    return render(request,"contact.html")
+# def about(request):
+#     return render(request,"about.html")
+# def contact(request):
+#     return render(request,"contact.html")
+
+def index(request):
+    return render(request,"index.html")
+def create(request):
+    return render(request,"create.html")
